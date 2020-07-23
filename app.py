fd8454cdcf58178bea3d7d4ee822837a0bb0c800
camun <munoz.leiva.c@gmail.com>
2020-07-23 16:46:23 -0400

    cambiando estructura renderizacion y imagenes

--- app.py
+++ app.py
@@ -19,11 +19,14 @@ dataDeficit = [
 	["Deficit","amg_puentenegro",97.4, 178.0, 0]
 ]
 
+
 @app.route('/')
 def index():
 	result = os.system("phantomjs generateImage.js")
 	print(result)
-	return render_template('index.html',dataAlert=dataAlert,dataDeficit=dataDeficit)
+	img_path = "http://localhost:5000/static/map.png"
+
+	return render_template('index.html',dataAlert=dataAlert,dataDeficit=dataDeficit,img_path=img_path)
 
 
 @app.route('/leaft')
@@ -34,11 +37,15 @@ def leaft():
 
 @app.route('/convert')
 def konversi():
-	#result = os.system("phantomjs generateImage.js")
-	#print(result)
-	htmlfile = app.config['TEMPLATE_FOLDER'] + 'index.html'
+	img_path = "http://localhost:5000/static/map.png"
+
+	## render html and save
+	f = open("templates/index2.html", "w")
+	f.write(render_template('index.html', dataAlert=dataAlert,dataDeficit=dataDeficit,img_path=img_path))
+	f.close()
+
+	htmlfile = app.config['TEMPLATE_FOLDER'] + 'index2.html'
 	pdffile = app.config['PDF_FOLDER'] + 'demo.pdf'
-	
 
 	pdfkit.from_file(htmlfile, pdffile)
 	return '''Click here to open the
